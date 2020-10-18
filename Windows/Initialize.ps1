7a57bb44ba4458ac44a24b0cb98fb30728628fc2
Ismael Serrada <ismael.serrada@gmail.com>
2020-10-17 22:51:00 -0300

    Add files for initial windows setup

---
+++ Windows/Initialize.ps1
@@ -0,0 +1,32 @@
+﻿function InstallPackages {
+    ./ChocoPackages.ps1
+}
+
+function InstallChocolatey {
+    ./InstallChocolatey.ps1
+}
+
+function GivePermissionToInstallPackages{
+    Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+}
+
+function RemovePermissionToInstallPackages {
+    Set-ExecutionPolicy Restricted
+}
+
+
+## Check if Chocolatey is installed
+
+Write-Host "... Checking if Chocolatey is Install" -ForegroundColor Yellow
+
+If(Test-Path -Path "$env:ProgramData\Chocolatey") {
+    Write-Host "Chocolatey is Install in computer (~‾▿‾)~" -ForegroundColor Green
+    InstallPackages;
+} Else {
+    InstallChocolatey;
+    InstallPackages;
+}
+
+## Remove permission in powershell to run scripts. Powershell must be run in administration mode
+RemovePermissionToInstallPackages;
+
